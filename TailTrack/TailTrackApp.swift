b9eed0f93acca0100a2d1daf1ddd985e3cfab21e
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-26 02:23:58 +0700

    Initial Commit

---
+++ TailTrack/TailTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TailTrackApp.swift
+//  TailTrack
+//
+//  Created by Michelle Swastika on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TailTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
