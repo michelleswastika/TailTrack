6569c26387829c583fba5cdec01127916a363aff
nale <89299208+michellealvera@users.noreply.github.com>
2023-05-26 09:43:27 +0700

    Dummy Data & Home View Added
    - HomeView Added
    - Dummy Data Added
    - Assets Updated

---
+++ TailTrack/Extensions/Dummies.swift
@@ -0,0 +1,16 @@
+//
+//  Dummies.swift
+//  TailTrack
+//
+//  Created by MacBook Pro on 26/05/23.
+//
+
+import Foundation
+
+let countryList = [
+    "Blacky",
+    "Doggo",
+    "Amor",
+    "Icel",
+    "Catty"
+]
---
+++ TailTrack/Views/PageView/HomeView.swift
@@ -0,0 +1,105 @@
+//
+//  HomeView.swift
+//  TailTrack
+//
+//  Created by MacBook Pro on 25/05/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var email: String = ""
+    
+    let countries = countryList
+    
+    var body: some View {
+        
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+            
+            NavigationStack {
+                
+                VStack {
+                    
+                    HStack {}
+                    .padding(.top)
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                
+                            }){
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                    }
+                    
+                    //                    Text("Home")
+                    //                        .font(.title)
+                    //                        .bold()
+                    //                        .padding(.top)
+                    //
+                    //
+                    //                    HStack {
+                    //                        Image(systemName: "plus")
+                    //                            .resizable()
+                    //                            .frame(width: 20, height: 20)
+                    //                        Text("Tambah")
+                    //                            .font(.title3)
+                    //                        Spacer()
+                    //                    }
+                    //                    .padding(.horizontal)
+                    //                    .foregroundColor(.blue)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        HStack {
+                            TextField("Search", text: $email)
+                            Image(systemName: "magnifyingglass")
+                        }
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)))
+                                .foregroundColor(.black)
+                            
+                        )
+                        .padding(.top)
+                    }
+                    .padding(.horizontal)
+                    
+                    
+                    List {
+                        ForEach(countries, id: \.self) { country in
+                            NavigationLink(destination: Text(country)) {
+                                Image("vera sip")
+                                    .resizable().frame(width: 62, height: 60)
+                                VStack (alignment: .leading) {
+                                    Text(country)
+                                    Text("Anak kucing")
+                                        .font(.footnote)
+                                        .foregroundColor(.gray)
+                                }
+                            }.padding()
+                        }
+                    }
+                    .listStyle(.plain)
+                    
+                    
+                }
+            }
+        }
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
