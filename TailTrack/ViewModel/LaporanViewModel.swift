9ba2ecb127080a30d07ef853080ef84f80ef244f
Davina T W <91788095+dolaemonngeong@users.noreply.github.com>
2023-05-27 22:16:54 +0700

    View Model Report & update model

--- TailTrack/ViewModel/LaporanViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LaporanViewModel.swift
-//  TailTrack
-//
-//  Created by Davina Teresa Wijaya on 22/05/23.
-//
-
-import Foundation
---
+++ TailTrack/ViewModel/ReportViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  LaporanViewModel.swift
+//  TailTrack
+//
+//  Created by Davina Teresa Wijaya on 22/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+    @MainActor class ReportViewModel: ObservableObject{
+        @Published private (set) var reports = [Report]()
+//        @Published var status: String = "sedang mencari"
+        
+        
+        func addReport(petName: String, petType: String, petCharacteristics: Array<String>, petOwner: String, ownersPhone: String, lastLocation: String, lastDate: Date, status: String, petPhoto: String){
+            let newReport = Report(petName: petName, petType: petType, petCharacteristics: petCharacteristics, petOwner: petOwner, ownersPhone: ownersPhone, lastLocation: lastLocation, lastDate: lastDate, status: status, petPhoto: petPhoto)
+            reports.append(newReport)
+        }
+        
+        func validateREport(){
+            
+        }
+        
+    }
