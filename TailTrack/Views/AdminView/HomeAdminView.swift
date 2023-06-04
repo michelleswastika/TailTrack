fba206f51976019ab2764d3eb736985188289133
nale <93458378+nextrektor@users.noreply.github.com>
2023-06-04 20:53:37 +0700

    AdminViewUpdated

--- TailTrack/Views/AdminView/HomeAdminView.swift
+++ TailTrack/Views/AdminView/HomeAdminView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct HomeAdminView: View {
     @State private var email: String = ""
-    
-    let countries = countryList
+    @StateObject private var reportViewModel = ReportViewModel()
     
     var body: some View {
         
@@ -44,21 +43,46 @@ struct HomeAdminView: View {
                     .padding(.horizontal)
                     
                     
-                    List {
-                        ForEach(countries, id: \.self) { country in
-                            NavigationLink(destination: Text(country)) {
-                                Image("vera sip")
-                                    .resizable().frame(width: 62, height: 60)
-                                VStack (alignment: .leading) {
-                                    Text(country)
-                                    Text("Anak kucing")
-                                        .font(.footnote)
-                                        .foregroundColor(.gray)
+                    List(reportViewModel.reports) { report in
+                        NavigationLink(destination: ReportDetailView(report: report)) {
+                            if let imageURL = reportViewModel.getImageURL(for: report) {
+                                AsyncImage(url: imageURL) { phase in
+                                    switch phase {
+                                    case .empty:
+                                        // Placeholder or loading view
+                                        ProgressView()
+                                    case .success(let image):
+                                        // Display the image
+                                        image
+                                            .resizable()
+                                            .frame(width: 62, height: 60)
+                                    case .failure:
+                                        // Error view or fallback image
+                                        Text("Failed to load image")
+                                    @unknown default:
+                                        // Handle future cases if needed
+                                        Text("Failed to load image")
+                                    }
                                 }
-                            }.padding()
+                            } else {
+                                Image("logo1024")
+                                    .resizable()
+                                    .frame(width: 62, height: 60)
+                            }
+                            
+                            VStack (alignment: .leading) {
+                                Text(report.petName)
+                                Text(report.petType + " milik " + report.petOwner)
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }
                         }
+                        .padding()
                     }
                     .listStyle(.plain)
+                    .onAppear {
+                        reportViewModel.fetchItems()
+                    }
                     
                     
                 }
